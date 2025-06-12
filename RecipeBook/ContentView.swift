dd3b3de953ca2266ec7e60651d7d30ba92ff0680
nqnphuong <nqnphuong2002@gmail.com>
2025-06-12 11:20:19 +0700

    Initial Commit

---
+++ RecipeBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeBook
+//
+//  Created by Mac on 12/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeBook/RecipeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookApp.swift
+//  RecipeBook
+//
+//  Created by Mac on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
